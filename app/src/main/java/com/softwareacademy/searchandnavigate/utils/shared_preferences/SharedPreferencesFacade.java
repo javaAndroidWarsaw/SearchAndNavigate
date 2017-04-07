49b99239bb18ec5f59cf70b7a6f4a9444e559181
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2017-04-07 15:06:28 +0200

    add retrieve object from shared preferences

--- app/src/main/java/com/softwareacademy/searchandnavigate/utils/shared_preferences/SharedPreferencesFacade.java
+++ app/src/main/java/com/softwareacademy/searchandnavigate/utils/shared_preferences/SharedPreferencesFacade.java
@@ -8,4 +8,7 @@ public interface SharedPreferencesFacade {
 
     void writeString(String value, String key);
     String getString(String key);
+
+    void putObject(String key, Object any);
+    <T> T getObject(String key, Class<T> object);
 }
--- app/src/main/java/com/softwareacademy/searchandnavigate/utils/shared_preferences/SharedPreferencesFacadeImpl.java
+++ app/src/main/java/com/softwareacademy/searchandnavigate/utils/shared_preferences/SharedPreferencesFacadeImpl.java
@@ -2,6 +2,9 @@ package com.softwareacademy.searchandnavigate.utils.shared_preferences;
 
 import android.content.SharedPreferences;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import javax.inject.Inject;
 
 /**
@@ -11,7 +14,7 @@ import javax.inject.Inject;
 public class SharedPreferencesFacadeImpl implements SharedPreferencesFacade {
 
     private SharedPreferences sharedPreferences;
-
+    private SharedPreferences.Editor editor;
     @Inject
     public SharedPreferencesFacadeImpl(SharedPreferences sharedPreferences) {
         this.sharedPreferences = sharedPreferences;
@@ -19,15 +22,31 @@ public class SharedPreferencesFacadeImpl implements SharedPreferencesFacade {
 
     @Override
     public void writeString(String value, String key) {
-
+        editor = sharedPreferences.edit();
+        editor.putString(key, value);
+        editor.apply();
     }
 
     @Override
     public String getString(String key) {
-        return null;
+        return sharedPreferences.getString(key, "");
     }
 
+    @Override
+    public void putObject(String key, Object any) {
+        Gson gson = new GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+        writeString(key, gson.toJson(any));
+    }
 
+    @Override
+    public <T> T getObject(String key, Class<T> object) {
+        Gson gson = new GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+        return gson.fromJson(sharedPreferences.getString(key, ""), object);
+    }
 
 
 }
