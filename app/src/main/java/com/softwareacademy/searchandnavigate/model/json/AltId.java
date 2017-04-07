26f43e140bb9067d4e1779b754b7007263145579
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2017-04-07 20:03:27 +0200

    add classes from google place Api

---
+++ app/src/main/java/com/softwareacademy/searchandnavigate/model/json/AltId.java
@@ -0,0 +1,32 @@
+
+package com.softwareacademy.searchandnavigate.model.json;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class AltId {
+
+    @SerializedName("place_id")
+    @Expose
+    private String placeId;
+    @SerializedName("scope")
+    @Expose
+    private String scope;
+
+    public String getPlaceId() {
+        return placeId;
+    }
+
+    public void setPlaceId(String placeId) {
+        this.placeId = placeId;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+}
