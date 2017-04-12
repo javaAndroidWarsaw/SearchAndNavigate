00a0f991a407e8b6c38d471e3c2fa6fe0e654ceb
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2017-04-12 15:14:38 +0200

    remove loggin  interceptor

--- app/src/main/java/com/softwareacademy/searchandnavigate/network/GoogleRetrofitProvider.java
+++ app/src/main/java/com/softwareacademy/searchandnavigate/network/GoogleRetrofitProvider.java
@@ -1,15 +1,11 @@
 package com.softwareacademy.searchandnavigate.network;
 
-import android.util.Log;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.softwareacademy.searchandnavigate.BuildConfig;
 
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -31,20 +27,11 @@ public class GoogleRetrofitProvider {
         OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
         OkHttpClient okHttpClient;
 
-        if (BuildConfig.DEBUG) {
-            HttpLoggingInterceptor.Logger myLogger = message -> Log.d("SEARCH_AND_NAVIGATE", message);
-            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor(myLogger);
-            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-            okHttpClient = httpClientBuilder
-//                    .addInterceptor(loggingInterceptor)
-                    .readTimeout(20, TimeUnit.SECONDS)
-                    .build();
-        } else {
-            okHttpClient = httpClientBuilder
-                    .readTimeout(20, TimeUnit.SECONDS)
-                    .build();
 
-        }
+        okHttpClient = httpClientBuilder
+                .readTimeout(20, TimeUnit.SECONDS)
+                .build();
+
         Gson gson1 = new GsonBuilder()
                 .excludeFieldsWithoutExposeAnnotation()
                 .setDateFormat("yyyy-MM-dd HH:mm:ssZ").create();
